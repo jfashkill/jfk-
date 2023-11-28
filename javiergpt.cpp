8baeecd2bdde1230c08cea45efb52412cbdc3c9c
Javi Giler <javigiler@Javis-MBP.home>
2023-11-27 21:45:49 -0600

    yo2

---
+++ javiergpt.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+// Replace 'YOUR_API_KEY' with your actual OpenAI GPT API key
+const std::string API_KEY = "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";
+const std::string OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+
